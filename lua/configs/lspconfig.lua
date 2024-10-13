07f2084c9ba8dfbd228fe2c9c6bcf4c0cc1f35e9
Denis Glotov <denis@glotov.org>
2024-10-13 12:08:34 +0300

    My initial changes

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,20 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+local servers = {
+  "clangd",
+  "cssls",
+  "gopls",
+  "jsonls",
+  "html",
+  "lua_ls",
+  "pylsp",
+  "rust_analyzer",
+  "solidity_ls",
+  "textlsp",
+  "ts_ls",
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -21,4 +34,18 @@ end
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
+-- }
+
+-- lspconfig.typos_lsp.setup {
+--   -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+--   cmd_env = { RUST_LOG = "error" },
+--   init_options = {
+--     -- Custom config. Used together with a config file found in the workspace or its parents,
+--     -- taking precedence for settings declared in both.
+--     -- Equivalent to the typos `--config` cli argument.
+--     config = "~/typos.toml",
+--     -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+--     -- Defaults to error.
+--     diagnosticSeverity = "Error",
+--   },
 -- }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -13,6 +13,39 @@ return {
     end,
   },
 
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup {}
+      local cmp = require "cmp"
+      local sources = cmp.get_config().sources
+      table.insert(sources, 1, { name = "supermaven" })
+      cmp.setup { sources = sources }
+    end,
+  },
+
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   -- cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup {}
+  --   end,
+  -- },
+
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+  },
+
+  {
+    "knsh14/vim-github-link",
+    config = function()
+      -- Optional: You can set up key mappings or additional configurations here
+      vim.api.nvim_set_keymap("n", "<leader>gl", ":GetCommitLink<CR>", { noremap = true, silent = true })
+    end,
+    event = "InsertEnter",
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
